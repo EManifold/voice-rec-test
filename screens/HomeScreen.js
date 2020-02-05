a51be177fe3edcb577c742c6b338e5423092c01c
Ellie <elliemmanifold@gmail.com>
2020-02-05 11:53:56 +0000

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,210 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { MonoText } from '../components/StyledText';
+
+import React from 'react';
+import {StyleSheet, Text, View, Button, AppRegistry} from 'react-native;
+import Voice from 'react-native-voice';
+
+export default class HomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+           recognized: '',
+           started: '',
+           results: [],
+       };
+
+        Voice.onSpeechStart = this.onSpeechStart.bind(this)
+        Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this)
+        Voice.onSpeechResults = this.onSpeechResults.bind(this)
+    }
+
+  onSpeechStart(e) {
+    this.setState({
+      started: '√',
+    });
+  }
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: '√',
+    });
+  }
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value,
+    });
+  }
+  async _startRecognition(e) {
+    this.setState({
+      recognized: '',
+      started: '',
+      results: [],
+    });
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render () {
+    return (
+      <View>
+        <Text style={styles.transcript}>
+            Transcript
+        </Text>
+        {this.state.results.map((result, index) => <Text style={styles.transcript}> {result}</Text>
+        )}
+        <Button style={styles.transcript}
+        onPress={this._startRecognition.bind(this)}
+        title="Start"></Button>
+      </View>
+    );
+  }
+
+}
+AppRegistry.registerComponent('HomeScreen', () => HomeScreen);
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+function DevelopmentModeNotice() {
+  if (__DEV__) {
+    const learnMoreButton = (
+      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
+        Learn more
+      </Text>
+    );
+
+    return (
+      <Text style={styles.developmentModeText}>
+        Development mode is enabled: your app will be slower but you can use
+        useful development tools. {learnMoreButton}
+      </Text>
+    );
+  } else {
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode: your app will run at full speed.
+      </Text>
+    );
+  }
+}
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/development-mode/'
+  );
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
+  );
+}
+
+const styles = StyleSheet.create({
+  transcript: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+    top: '400%',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
+  getStartedContainer: {
+    alignItems: 'center',
+    marginHorizontal: 50,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
+  },
+  codeHighlightText: {
+    color: 'rgba(96,100,109, 0.8)',
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+    alignItems: 'center',
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 20,
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    textAlign: 'center',
+  },
+  navigationFilename: {
+    marginTop: 5,
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: 'center',
+  },
+  helpLink: {
+    paddingVertical: 15,
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: '#2e78b7',
+  },
+});
